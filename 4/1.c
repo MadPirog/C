20654a3738b4e06c2ebab6f530fb78c22eb5418b
MadPirog <fazliev07082009@gmail.com>
2025-04-12 12:20:34 +0300

    Update 1.c

--- 4/1.c
+++ 4/1.c
@@ -3,7 +3,7 @@
 int main() {
   int red_balls[10];
   for (int i = 0; i < 10; i++)
-    red_balls[i] = i + 2
+    red_balls[i] = i + 2;
   int size = sizeof(red_balls) / sizeof(red_balls[0]);
   printf("В коробке red_balls помещается %d шаров.\n", size);
   return 0;
